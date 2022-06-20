413311cba522b968fa8df0e9dc2f413d46720831
Abdulkadry mntambo <mntambo.abdul@gmail.com>
2022-06-20 15:28:32 -0400

    New File

---
+++ verification.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+####This is a Hellow world Dipslay Script 
+
+#### Authour : Engineer Mntambo, Abdulkadry I
+
+echo " Hello World "
+read Response 
+
+echo " Hello World ${Response} "
+
+echo " Please Enter Your Age"
+read Age
+
+if 
+[ $Age -eq 18]
+
+echo " Welcome and Have fun "
+
+else 
+
+echo " Please only for the Elders"
+
+fi
